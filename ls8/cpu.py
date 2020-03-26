5a2011223a4aa61c9e722bdecef04c6ea2f7f55a
Pat Palmerston <patpalmerston@gmail.com>
2020-03-26 08:50:04 -0700

    day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,97 +8,98 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        # 256 bytes of memory
         self.ram = [0] * 256
+        # 8 general-purpose registers
         self.reg = [0] * 8
+        # PC
         self.pc = 0
-        # stack pointer
+        # stack pointer (SP)
         self.sp = 256
+
+        # self.mdr = 0
+        # Commands
         self.commands = {
             0b00000001: self.hlt,
             0b10000010: self.ldi,
             0b01000111: self.prn,
             0b10100010: self.mul,
             0b01000101: self.push,
-            0b01000110: self.pop
+            0b01000110: self.pop,
+            0b10100000: self.add
         }
-    # takes the address of RAM returns that value
 
+    # accepts the address in RAM and returns the value stored there
     def ram_read(self, address):
         return self.ram[address]
 
+    # accepts a value to write, and the address to write it to.
     def ram_write(self, value, address):
         self.ram[address] = value
-    # used to halt cpu and exit the emulation
 
-    def hlt(self, op_a, op_b):
-        return(0, False)
-    # loading current store of
+    # halt the CPU and exit the emulator
+    def hlt(self, operand_a, operand_b):
+        return (0, False)
 
-    def ldi(self, op_a, op_b):
-        self.reg[op_a] = op_b
+    # load immediate, store a value in a register, or set this register to this value
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
         return (3, True)
 
-    def prn(self, op_a, op_b):
-        print(self.reg[op_a])
+    # prints the numeric value stored in a register
+    def prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
         return (2, True)
 
-    def mul(self, op_a, op_b):
-        # call alu
-        self.alu("MUL", op_a, op_b)
+    # multiply the values in two registers together and store the result in registerA
+    def mul(self, operand_a, operand_b):
+        # calling alu function
+        self.alu("MUL", operand_a, operand_b)
         return (3, True)
 
-    def add(self, op_a, op_b):
-        # call alu
-        self.alu("ADD", op_a, op_b)
+    def add(self, operand_a, operand_b):
+        # calling alu function
+        self.alu("ADD", operand_a, operand_b)
         return (3, True)
 
-    # push
-    def push(self, op_a, op_b):
-        push_reg = self.ram[self.pc + 1]
-        self.pc -= 1
-        val = self.reg[push_reg]
-        self.ram[self.sp] = val
+    def push(self, operand_a, operand_b):
+        # reg_address = self.ram[self.pc + 1]
+        self.sp -= 1
+        value = self.reg[operand_a]
+        self.ram[self.sp] = value
         return (2, True)
 
-    # pop
-    def pop(self, op_a, op_b):
-        pass
+    def pop(self, operand_a, operand_b):
+        pop_value = self.ram[self.sp]
+        reg_address = operand_a
+        self.reg[reg_address] = pop_value
+        self.sp += 1
+        return (2, True)
 
-    # call
-
-    # iret
+    # loading from a file
 
     def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
         with open(program) as f:
             for line in f:
                 comment_split = line.split('#')
                 num = comment_split[0].strip()
 
+                if num == '':
+                    continue
+
                 try:
+                    # print(num)
                     self.ram_write(int(num, 2), address)
                     address += 1
                 except ValueError:
-                    print('Value Error')
+                    # print("Value error")
                     pass
+
+        # print("Finished reading the program")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -133,21 +134,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        # read memory in reg and stor in 'IR'
         running = True
 
         while running:
+            instruction_register = self.ram_read(self.pc)
+            # print(instruction_register)
 
-            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
-            op_a = self.ram_read(self.pc + 1)
-            op_b = self.ram_read(self.pc + 2)
+            # print(instruction_register, operand_a, operand_b)
 
             try:
-                operation_output = self.commands[ir](op_a, op_b)
-                running = operation_output[1]
-                self.pc += operation_output[0]
+                f = self.commands[instruction_register]
+                # print(f)
+                operation_op = f(operand_a, operand_b)
+                running = operation_op[1]
+                self.pc += operation_op[0]
 
-            except:
-                print(f"not working: {ir}")
-                sys.exit()
+            except Exception as e:
+                print(f"Error: Instruction {instruction_register} not found!")
+                # print(e)
+                sys.exit(1)
